3115001b6c9db9c16dcc26323413da9ce93f939c
IBYIMANA <filletteibyimana@gmail.com>
2024-04-13 11:01:22 +0200

    sec

--- src/contexts/ContactsContext.js
+++ src/contexts/ContactsContext.js
@@ -8,7 +8,7 @@ export const ContactsContextProvider = ({ children }) => {
     const [contacts, setContacts] = useState([
         {
             id: 1,
-            fullName: "Marie Louise",
+            fullName: "Ibyimana fillette",
             profile: "https://media.istockphoto.com/id/1300845620/vector/user-icon-flat-isolated-on-white-background-user-symbol-vector-illustration.jpg?s=612x612&w=0&k=20&c=yBeyba0hUkh14_jgv1OKqIH0CCSWU_4ckRkAoy2p73o="
         },
         {
