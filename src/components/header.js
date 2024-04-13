d6c5a617b4685fc0093f8c7a578a8b1eb8fd1681
IBYIMANA <filletteibyimana@gmail.com>
2024-04-13 11:24:11 +0200

    four

--- src/components/header.js
+++ src/components/header.js
@@ -4,7 +4,7 @@ import { CgProfile } from "react-icons/cg";
 export const Header = () => {
     return (
         <div>
-            <div className='bg-slate-300 w-full h-24 flex flex-row justify-around items-center shadow-[rgba(0,_0,_0,_0.24)_0px_3px_8px]'>
+            <div className='flex flex-row items-center justify-around w-full h-24 bg-gray-300'>
                 <h1 className='text-2xl font-bold'>Contact</h1>
                 <h1 className='font-bold'>Home</h1>
                 <div className='flex flex-row items-center px-4 py-2 space-x-2 rounded-lg bg-lime-500' >
@@ -13,7 +13,7 @@ export const Header = () => {
                         <input type="text" className='text-black bg-lime-500'></input>
                         <input type="text" className='text-black bg-lime-500' ></input>
                     </div>
-                    <button className='bg-black text-white font-bold px-4 py-2 rounded-lg shadow-[0_10px_20px_rgba(240,_46,_170,_0.7)]'>Create Contact</button>
+                    <button className='px-4 py-2 font-bold text-black bg-gray-400 rounded-lg'>Create Contact</button>
                 </div>
             </div>
         </div>
